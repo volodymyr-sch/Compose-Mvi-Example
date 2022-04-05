cf1e80ae8d355d39cedf01b76c444d7b87f1e527
volodymyrsch <vladimirscherbyuk@gmail.com>
2022-04-05 21:10:23 +0300

    - Update libraries version - Update kotlin plugin version - Update gradle plugin version - Improve code base

--- app/src/main/java/com/vsch/mvi/presentation/main/MainScreenViewDataModel.kt
+++ app/src/main/java/com/vsch/mvi/presentation/main/MainScreenViewDataModel.kt
@@ -1,12 +1,9 @@
 package com.vsch.mvi.presentation.main
 
 sealed class MainScreenItem {
-
     object MainScreenAddButtonItem : MainScreenItem()
-
     data class MainScreenTodoItem(
         val isChecked: Boolean,
         val text: String,
     ) : MainScreenItem()
-
 }
